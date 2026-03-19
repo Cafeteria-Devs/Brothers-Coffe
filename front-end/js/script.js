f68bb1b66e4a1881de1219c1382a5b5873a2fb49
ViniciusAnonimus007 <contato.evertonbarreto.dev@gmail.com>
2026-03-19 14:18:49 -0300

    feat(ux/ui): refatora o header mobile e adiciona perfil de usuário

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -1,8 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const mobileMenu = document.getElementById("mobile-menu");
+    const navLinks = document.querySelector(".nav-links");
+
+    if (mobileMenu && navLinks) {
+        mobileMenu.addEventListener("click", () => {
+            navLinks.classList.toggle("active");
+            mobileMenu.classList.toggle("toggle");
+            
+            if (navLinks.classList.contains("active")) {
+                document.body.style.overflow = "hidden";
+            } else {
+                document.body.style.overflow = "initial";
+            }
+        });
+
+        const navItems = document.querySelectorAll(".nav-links a");
+        navItems.forEach(item => {
+            item.addEventListener("click", (e) => {
+                navLinks.classList.remove("active");
+                mobileMenu.classList.remove("toggle");
+                document.body.style.overflow = "initial";
+            });
+        });
+    }
+
     window.fade = function(event) {
         event.preventDefault();
-        
         const destino = event.currentTarget.href;
+        
         document.body.classList.add("fade-out");
         
         setTimeout(() => {
@@ -10,16 +35,3 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 500);
     };
 });
-
-document.addEventListener("DOMContentLoaded", () => {
-    const mobileMenu = document.getElementById("mobile-menu");
-    const navLinks = document.querySelector(".nav-links");
-
-    mobileMenu.addEventListener("click", () => {
-        navLinks.classList.toggle("active");
-        
-        // Opcional: Animação simples no ícone hambúrguer
-        mobileMenu.classList.toggle("toggle");
-    });
-});
-
