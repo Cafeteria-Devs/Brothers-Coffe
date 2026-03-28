474aa00c4808dc3cd17ec0a5092fcce800c54c6a
ViniciusAnonimus007 <contato.evertonbarreto.dev@gmail.com>
2026-03-28 12:06:06 -0300

    chore: atualiza projeto para arquitetura SPA

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        // Liste aqui todas as suas páginas
-        main: resolve(__dirname, 'index.html'),
-        login: resolve(__dirname, 'pages/login.html'),
-        cardapio: resolve(__dirname, 'pages/cardapio.html'),
-        restaurantes: resolve(__dirname, 'pages/restaurantes.html'),
-        contatos: resolve(__dirname, 'pages/contatos.html'),
-        apoiadores: resolve(__dirname, 'pages/apoiadores.html'),
-        comprar: resolve(__dirname, 'pages/comprar.html'),
-      },
-    },
-  },
+  plugins: [react()],
+  // Sem bloco server = Vite escolhe a porta padrão dele
 })
