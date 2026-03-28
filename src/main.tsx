2328b67c1b0529f8077ffdd7298942c9a3efe87a
ViniciusAnonimus007 <contato.evertonbarreto.dev@gmail.com>
2026-03-28 13:54:02 -0300

    feat: inicializa react e react routes

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import '../styles/media/mobile.css'
+import '../styles/media/tablet.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
