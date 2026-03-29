c25bd557465848668b4336e98d4b8e0785171c3c
ViniciusAnonimus007 <contato.evertonbarreto.dev@gmail.com>
2026-03-29 10:04:15 -0300

    refactor: página das lojas em react

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Header } from './components/Header'
 import { Footer } from './components/Footer'
 import Home from './pages/Home'
 import DevTeam from './pages/DevTeam'
+import Stores from './pages/Stores'
+
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/apoiadores" element={<DevTeam />} />
+        <Route path="/restaurantes" element={<Stores />} />
       </Routes>
 
       <Footer />
---
+++ src/data/store.ts
@@ -0,0 +1,27 @@
+import { Store } from "../types/Store";
+
+const storesData: Store[] = [
+  {
+    id: 1,
+    name: "Brothers Coffee Candeias",
+    address: "Rua Iguatemi - Candeias",
+    cityState: "Jaboatão dos Guararapes / PE",
+    phoneLink: "https://criarmeulink.com.br/u/1769349921",
+  },
+  {
+    id: 2,
+    name: "Brothers Coffee Piedade",
+    address: "Endereço em breve",
+    cityState: "Jaboatão dos Guararapes / PE",
+    phoneLink: "https://criarmeulink.com.br/u/1769349921",
+  },
+  {
+    id: 3,
+    name: "Brothers Coffee Cajueiro Seco",
+    address: "Endereço em breve",
+    cityState: "Jaboatão dos Guararapes / PE",
+    phoneLink: "https://criarmeulink.com.br/u/1769349921",
+  },
+];
+
+export default storesData
