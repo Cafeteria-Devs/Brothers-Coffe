0549105e199a5fb2597fcb490744f64423135610
ViniciusAnonimus007 <contato.evertonbarreto.dev@gmail.com>
2026-04-12 11:41:55 -0300

    refactor(security): lista produtos por função
    - produtos agora são carregados pela edge function
    - retira a possibilidade momentanea de pegar a tabela de stock

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import productsData from "../data/products";
+import { getProducts } from "../services/products";
 import { Product } from "../types/Product";
 import "../../styles/cardapio.css"
 import "../../styles/media/mobile.css"
@@ -10,8 +10,9 @@ const Products = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const products = await productsData();
+      const products = await getProducts();
       setData(products);
+      console.warn(products)
     };
     fetchData();
   }, []);
